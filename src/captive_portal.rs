ab83ecd6b0ad84fa0042d77026e86c8fdd7e61aa
SyntaxError <294848543+ValueMovedHere@users.noreply.github.com>
2026-09-06 12:44:45 +0800

    Start captive portal web server

--- src/captive_portal.rs
+++ src/captive_portal.rs
@@ -1,7 +1,15 @@
+use std::path::Path;
 use std::process::Command;
 
-fn start_server() {
+fn start_server(server_path: &Path, pages_path: &Path) {
     // 启动强制门户网页服务器，并且使用指定的钓鱼页面
+    Command::new(server_path)
+        .arg("-p")
+        .arg("80")
+        .arg("-n")
+        .arg("-o")
+        .spawn()
+        .expect("Failed to start the web server");
 }
 
 mod page_type {
