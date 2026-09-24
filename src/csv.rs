518429828cdbd2e384c15bd6611ab19dd3287baf
SyntaxError <294848543+ValueMovedHere@users.noreply.github.com>
2026-09-24 10:05:14 +0800

    modified:   src/csv.rs

--- src/csv.rs
+++ src/csv.rs
@@ -1,7 +1,7 @@
 // 从 airodump-ng 输出的 CSV 中解析与目标相关的信息
 
 use std::{fs::File, io::Seek}
-use std::io::{BufReader, Read};
+use std::io::{BufReader, Cursor, Read};
 use std::path::Path;
 
 use csv;
@@ -18,5 +18,7 @@ fn records(path: &Path) -> (Vec<ApRecord>, Vec<StationRecord>) {
     let mut file_string = String::new();
     reader.read_to_string(&mut file_string).unwrap();
     let parts: Vec<&str> = file_string.split("\n\n").collect();
+    let ap_records_reader = csv::Reader::from_reader(Cursor::new(parts[0].as_bytes()));
+    let station_records_reader = csv::Reader::from_reader(Cursor::new(parts[1].as_bytes()));
     (Vec<ApRecord>, Vec<StationRecord>)
 }
