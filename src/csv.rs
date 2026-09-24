b3c1804d141a91c22ff18c8ebb07ea4a7796d779
SyntaxError <294848543+ValueMovedHere@users.noreply.github.com>
2026-09-24 19:54:11 +0800

    Fix parse error

--- src/csv.rs
+++ src/csv.rs
@@ -15,14 +15,19 @@ pub fn records(path: &Path) -> (Vec<ApRecord>, Vec<StationRecord>) {
     let mut file_string = String::new();
     reader.read_to_string(&mut file_string).unwrap();
     let parts: Vec<&str> = file_string.split("\r\n\r\n").collect();
-    println!("{:#?}", parts);
-    let mut ap_records_reader = csv::Reader::from_reader(Cursor::new(parts[0].as_bytes()));
-    let mut station_records_reader = csv::Reader::from_reader(Cursor::new(parts[1].as_bytes()));
+    let mut ap_records_reader = csv::ReaderBuilder::new()
+        .trim(csv::Trim::All)
+        .from_reader(Cursor::new(parts[0].as_bytes()));
+    println!("{:#?}", parts[0]);
+    let mut station_records_reader = csv::ReaderBuilder::new()
+        .trim(csv::Trim::All)
+        .from_reader(Cursor::new(parts[1].as_bytes()));
     let mut ap_record_vec = Vec::with_capacity(5usize);
     for result in ap_records_reader.deserialize() {
         let ap_record = result.unwrap();
         ap_record_vec.push(ap_record);
     }
+    println!("=============Parsing station record data==============");
     let mut station_records_vec = Vec::with_capacity(5usize);
     for result in station_records_reader.deserialize() {
         let station_record = result.unwrap();
