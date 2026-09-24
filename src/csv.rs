796673476215207ea4f21a8ae4991d71101fedd0
SyntaxError <294848543+ValueMovedHere@users.noreply.github.com>
2026-09-24 16:57:36 +0800

    Parse csv file

--- src/csv.rs
+++ src/csv.rs
@@ -1,24 +1,31 @@
 // 从 airodump-ng 输出的 CSV 中解析与目标相关的信息
 
-use std::{fs::File, io::Seek}
 use std::io::{BufReader, Cursor, Read};
 use std::path::Path;
+use std::{fs::File, io::Seek};
 
 use csv;
-use serde::Deserialize;
 
 use crate::records::{ApRecord, StationRecord};
 
 fn records(path: &Path) -> (Vec<ApRecord>, Vec<StationRecord>) {
-    let file = File::open(path)
-        .unwrap();
-    file
-        .seek(std::io::SeekFrom::Start(1u64)).unwrap();
-    let reader = BufReader::new(file);
+    let mut file = File::open(path).unwrap();
+    file.seek(std::io::SeekFrom::Start(1u64)).unwrap();
+    let mut reader = BufReader::new(file);
     let mut file_string = String::new();
     reader.read_to_string(&mut file_string).unwrap();
     let parts: Vec<&str> = file_string.split("\n\n").collect();
-    let ap_records_reader = csv::Reader::from_reader(Cursor::new(parts[0].as_bytes()));
-    let station_records_reader = csv::Reader::from_reader(Cursor::new(parts[1].as_bytes()));
-    (Vec<ApRecord>, Vec<StationRecord>)
+    let mut ap_records_reader = csv::Reader::from_reader(Cursor::new(parts[0].as_bytes()));
+    let mut station_records_reader = csv::Reader::from_reader(Cursor::new(parts[1].as_bytes()));
+    let mut ap_record_vec = Vec::with_capacity(5usize);
+    for result in ap_records_reader.deserialize() {
+        let ap_record = result.unwrap();
+        ap_record_vec.push(ap_record);
+    }
+    let mut station_records_vec = Vec::with_capacity(5usize);
+    for result in station_records_reader.deserialize() {
+        let station_record = result.unwrap();
+        station_records_vec.push(station_record);
+    }
+    (ap_record_vec, station_records_vec)
 }
