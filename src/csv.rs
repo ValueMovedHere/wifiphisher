e165b5a7fe69f441f9d1137913d2b609edaa92fb
SyntaxError <294848543+ValueMovedHere@users.noreply.github.com>
2026-09-24 18:51:26 +0800

    modified:   src/csv.rs

--- src/csv.rs
+++ src/csv.rs
@@ -14,7 +14,8 @@ pub fn records(path: &Path) -> (Vec<ApRecord>, Vec<StationRecord>) {
     let mut reader = BufReader::new(file);
     let mut file_string = String::new();
     reader.read_to_string(&mut file_string).unwrap();
-    let parts: Vec<&str> = file_string.split("\n\n").collect();
+    let parts: Vec<&str> = file_string.split("\r\n\r\n").collect();
+    println!("{:#?}", parts);
     let mut ap_records_reader = csv::Reader::from_reader(Cursor::new(parts[0].as_bytes()));
     let mut station_records_reader = csv::Reader::from_reader(Cursor::new(parts[1].as_bytes()));
     let mut ap_record_vec = Vec::with_capacity(5usize);
